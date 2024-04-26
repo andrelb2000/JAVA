d51a8b03f602c157bcc26c1c360bd1d3c08c8a11
Prof. André Luiz Braga, D.Sc <andre.luiz.braga@terra.com.br>
2024-04-26 11:38:05 -0300

    IBMEC

---
+++ mavenAULA_NETBEANS_0001/src/main/java/Ocorrencia.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author andre
+ */
+public class Ocorrencia {
+    private String codigo;
+    private String hora;
+    public Ocorrencia(String codigo, String hora) {
+        this.codigo = codigo;
+        this.hora = hora;
+    }
+    
+    
+    
+    
+}
